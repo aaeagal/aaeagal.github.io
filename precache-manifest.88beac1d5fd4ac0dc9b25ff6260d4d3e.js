ecb4f627c4374687162a86061f92e0e0938241c2
aaeagal <azeeza.a.eagal@gmail.com>
2021-12-17 13:22:48 -0500

    Updates

---
+++ precache-manifest.88beac1d5fd4ac0dc9b25ff6260d4d3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba80ee42b78fa25758d8e796e0080a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf6df6264ac18cec6b2c",
+    "url": "/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "bc1e6462c5b7a9238ea2",
+    "url": "/static/js/2.c6919101.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c6919101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6df6264ac18cec6b2c",
+    "url": "/static/js/main.e0510e70.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "884e98a2f96e1f9eb1270e04fff96ea4",
+    "url": "/static/media/ncstate.884e98a2.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "803d9b2b61d5fcc909b90d426d8eab70",
+    "url": "/static/media/truman.803d9b2b.jpg"
+  },
+  {
+    "revision": "fc840ce1e449cfc1a4a45ae34de0a6f4",
+    "url": "/static/media/zeezah.fc840ce1.jpg"
+  }
+]);
